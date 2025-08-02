6178b761c4c4505cb4d3423d46dfc896ff09b9a3
Mostafa Said <m.ssaid356@gmail.com>
2025-08-02 07:30:28 +0300

    Update Customer.cs

--- BikeStoreDBwithLinq/Models/Customer.cs
+++ BikeStoreDBwithLinq/Models/Customer.cs
@@ -1,17 +1,29 @@
-﻿// Models/Customer.cs
+// Models/Customer.cs
+
+using System.ComponentModel.DataAnnotations;﻿
+
 namespace BikeStoreDBwithLinq.Models
 {
     public class Customer
     {
+        [Key]
         public int CustomerId { get; set; }
+        
         public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
+        [MaxLength(255)]
         public string? Phone { get; set; }
+        [MaxLength(255)]
         public string Email { get; set; } = null!;
+        [MaxLength(255)]
         public string Street { get; set; } = null!;
+        [MaxLength(50)]
         public string City { get; set; } = null!;
+        [MaxLength(50)]
         public string State { get; set; } = null!;
+        [MaxLength(25)]
         public string ZipCode { get; set; } = null!;
         public ICollection<Order> Orders { get; set; } = new List<Order>();
     }
 }
+
