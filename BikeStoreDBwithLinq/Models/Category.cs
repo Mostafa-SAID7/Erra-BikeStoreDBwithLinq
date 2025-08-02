7a02c6a37edbd777af32dfb3bdd650ad88020c1d
Mostafa Said <m.ssaid356@gmail.com>
2025-08-02 07:24:53 +0300

    Update Category.cs

--- BikeStoreDBwithLinq/Models/Category.cs
+++ BikeStoreDBwithLinq/Models/Category.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BikeStoreDBwithLinq.Models
 {
     public class Category
     {
+        [Key]
         public int CategoryId { get; set; }
 
-        public string CategoryName { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
 
         public ICollection<Product> Products { get; set; }
     }
 }
+
